05872da9a64d36af391b40afd1ed7f31481048f1
Ranjith Samala <samalaranjith1@gmail.com>
2023-03-15 06:42:34 +0530

    fc

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello Master , Please realize that no one is going to be with you forever ----MP
+
+      You are an ideal piece sent by that god ..., yes bad things will come to you but don't go away ..., 
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,15 @@
+import { ActionTypes } from "../constants/action-types"
+const initialState = {
+    products:[]
+}
+export const productReducer = (state, action) ={
+    switch (type) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+    
+}
